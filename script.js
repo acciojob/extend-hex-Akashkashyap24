887f3e38dc87cf55c963c5b7cd3542ab38333613
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2023-12-30 13:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+  let ans="#",l=shortHex.length,i=1;
+  while(i<=l){
+    ans=ans+shortHex[i]+shortHex[i];
+  }
+  return ans;
 };
-
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
